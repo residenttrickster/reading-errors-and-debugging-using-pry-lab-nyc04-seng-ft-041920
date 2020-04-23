6ba999494aba8d9350c85cf3a19efa6c28571b19
Anastasia Orlova <metkusblackus@gmail.com>
2020-04-23 18:43:40 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,9 @@
 require 'pry'
 
 def snake_it_up(string)
-  if string[0] == s
-    10 * s + string
-  else
-    string
-  end
-end
+  if string[0] == "s"
+    ("s" * 10) + string
+  else	  
+  string	    
+  end	  
+end	
